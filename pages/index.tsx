f363f72d1bc297ab8929b66e35a112f4928f9cf8
YousfiHamza1 <hamza@flowdigitalstudio.com>
2021-07-04 21:16:39 +0100

    cypress initial setups

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
+import Head from 'next/head'
 
 function Home() {
     return (
-        <div>
-            Hello From the HOME Page
-        </div>
+        <>
+            <Head>
+                <title>NEXT FLOW TRANSFORMATION</title>
+            </Head>
+            <div className="homePage">
+                <h1>Hello From The Other Side</h1>
+                <Image src={'/images/flowD.png'} alt="@Flow" height={123} width={123} />
+            </div>
+        </>
     )
 }
 
